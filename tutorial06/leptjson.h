8b218e9759697237bb69b082337f8294559b5475
unknown <djw749800@outlook.com>
2021-11-23 16:56:57 +0800

    解析器完成，没想到一个*号让人debug半天多

--- tutorial06/leptjson.h
+++ tutorial06/leptjson.h
@@ -46,6 +46,8 @@ int lept_parse(lept_value* v, const char* json);
 
 void lept_free(lept_value* v);
 
+void lept_member_free(lept_member* o);
+
 lept_type lept_get_type(const lept_value* v);
 
 #define lept_set_null(v) lept_free(v)
